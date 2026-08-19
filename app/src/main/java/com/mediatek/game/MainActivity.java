1fc9f1d7f7bb47e1b5d26c6206eb3435ea1ad9ec
Hazakura Club <105591857+HazakuraID@users.noreply.github.com>
2026-08-19 23:25:17 +0700

    Update MainActivity with complete frame rate implementation

--- app/src/main/java/com/mediatek/game/MainActivity.java
+++ app/src/main/java/com/mediatek/game/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
       setFrameRateApi31();
     }
     TextView v = new TextView(this);
-    v.setText("Game Helio G100\n\nAndroid 16 / API 36\n120 FPS frame-rate request\nMali-G57 MP2\n\nProfiles:\nQQ飞车: com.tencent.tmgp.speedmobile\nSpeed Drifters: com.garena.game.fctw\n\n120 FPS unlocked");
+    v.setText("Game Helio G100\n\nAndroid 16 / API 36\n120 FPS frame-rate request\nMali-G57 MP2\n\nProfiles:\nQQ飞车: com.tencent.tmgp.speedmobile\nSpeed Drifters: com.garena.game.fctw\n\n120 FPS");
     v.setTextSize(17); v.setPadding(36,56,36,36); setContentView(v);
   }
 
